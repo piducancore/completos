a8fa4b0ef89f257ee6b31bd6fae1c8affb0517cf
piducancore:/# <hola@piducan.dev>
2021-12-14 17:09:45 -0300

    update map

--- src/components/map.js
+++ src/components/map.js
@@ -1,42 +1,60 @@
 /** @jsx jsx */
-import { jsx } from "theme-ui"
+import { jsx, Themed } from "theme-ui"
+import MapGL, { FlyToInterpolator, GeolocateControl, FullscreenControl, NavigationControl, ScaleControl, Popup } from "react-map-gl"
+import mb from "mapbox-gl"
 import { useState } from "react"
-import MapGL, { GeolocateControl } from "react-map-gl"
+import { easeBounceOut } from "d3-ease"
+
 import "mapbox-gl/dist/mapbox-gl.css"
 
-import mb from "mapbox-gl"
+import useViewport from "../useViewport"
+
 mb.workerClass = require("worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker").default
 
-const MAPBOX_TOKEN = process.env.GATSBY_MAPBOX_TOKEN
-
-const geolocateStyle = {
-  top: 0,
-  left: 0,
-  margin: 10,
-}
-const positionOptions = { enableHighAccuracy: true }
-
 export default function Map() {
-  const [viewport, setViewport] = useState({
-    latitude: -35.43886,
-    longitude: -71.65988,
-    zoom: 14,
-    bearing: 0,
-    pitch: 48,
-  })
-
+  const { viewport, setViewport } = useViewport()
+  const [popup, setPopup] = useState(null)
+  const handleClick = e => {
+    let feat = e.features.length > 0 && e.features[0].layer["source-layer"] === "completosTalca" ? e.features[0] : false
+    if (feat) {
+      e.preventDefault()
+      setViewport({
+        ...viewport,
+        longitude: feat.geometry.coordinates[0],
+        latitude: feat.geometry.coordinates[1],
+        transitionDuration: 888,
+        transitionInterpolator: new FlyToInterpolator({ curve: 1.6, maxDuration: 888 }),
+        transitionEasing: easeBounceOut,
+      })
+      setPopup(feat)
+    }
+  }
   return (
-    <div sx={{ height: "80vh" }}>
-      <MapGL
-        {...viewport}
-        width="100%"
-        height="100%"
-        mapStyle="mapbox://styles/piducancore/ckvxvtnvy0iug15l8hcb207rm"
-        onViewportChange={setViewport}
-        mapboxApiAccessToken={MAPBOX_TOKEN}
-      >
-        <GeolocateControl style={geolocateStyle} positionOptions={positionOptions} trackUserLocation auto />
-      </MapGL>
-    </div>
+    <MapGL
+      {...viewport}
+      width="100%"
+      height="100%"
+      mapStyle="mapbox://styles/piducancore/ckvxvtnvy0iug15l8hcb207rm"
+      onViewportChange={setViewport}
+      mapboxApiAccessToken={process.env.GATSBY_MAPBOX_TOKEN}
+      onClick={handleClick}
+    >
+      {popup && (
+        <Popup
+          tipSize={5}
+          anchor="top"
+          longitude={popup.geometry.coordinates[0]}
+          latitude={popup.geometry.coordinates[1]}
+          closeOnClick={true}
+          onClose={setPopup}
+        >
+          <Themed.p>{popup.properties.title}</Themed.p>
+        </Popup>
+      )}
+      <GeolocateControl sx={{ margin: 10 }} positionOptions={{ enableHighAccuracy: true }} /* trackUserLocation auto */ />
+      <FullscreenControl sx={{ margin: 10, top: 36 }} />
+      <NavigationControl sx={{ margin: 10, top: 72 }} />
+      <ScaleControl sx={{ margin: 10, bottom: 36 }} />
+    </MapGL>
   )
 }
